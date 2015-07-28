ca28b44e76f7433ff20c5be2f0f4ed86a65620a0
Josh Benner <bennerster@gmail.com>
2015-07-28 02:39:20 -0400

    Writes to Files
    Writes to files and fixed incorrect locations

--- LinkedInScrapeApp.py
+++ LinkedInScrapeApp.py
@@ -6,6 +6,20 @@ from bs4 import BeautifulSoup
 import urllib, urllib3
 
 
+
+def writeInfo(users):
+    file = "triangleinfo.txt"
+    file_obj = open(file,'w')
+    for user in users:
+        print("Name: " + user[0],file=file_obj)
+        print("Current Location: " + user[1],file=file_obj)
+        for experiences in user[2]:
+            for k,v in experiences.items():
+                print("Company: " + k + " Location: " + v,file=file_obj)
+        print(file=file_obj)
+    file_obj.close()
+    print("file has been written to")
+
 def printInfo(users):
     for user in users:
         print("Name: " + user[0])
@@ -16,7 +30,7 @@ def printInfo(users):
         print()
         print()
         print()
-
+    
 def grabInfo(urls):
     users = []
     for x in urls:
@@ -34,21 +48,22 @@ def grabInfo(urls):
         section = company.findAll("h5")
         jobNums = BeautifulSoup(str(exp_lst))
         experience = []
+        i = 0
         for item in section:
             info = item.contents
             if str(info[0])[13:19] == "degree":
                 break
-            for x in range(0,len(info)):
-                #if("Michigan State UniversityBachelor" in x.text or "(M.S.)" in x.text or "B.S" in x.text):
-                if info[x].text != "" :
-                    experience.append({str(info[x].text):places[x].text})
+            if info[0].text != "" :
+                experience.append({str(info[0].text):places[i].text})
+                i += 1
         user.append(experience)
         users.append(user)
-    printInfo(users)
+    writeInfo(users)
 
 headers = { 'User-Agent' : 'Mozilla/5.0' }
 urls = ["https://www.linkedin.com/in/travisjpacker","https://www.linkedin.com/pub/nicholas-mirallegro/89/845/128","https://www.linkedin.com/in/joshbenner851"]
 #urls = ["https://www.linkedin.com/in/joshbenner851"]
+#urls = ["https://www.linkedin.com/pub/william-dion/43/774/753"]
 grabInfo(urls)
 
 
